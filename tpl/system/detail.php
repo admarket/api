42a52b37e84dc29439b4deb6ecf1583dbc7d17e3
admarket <4006776@qq.com>
2015-04-23 23:45:43 +0800

    update something
    update detail page

--- tpl/system/detail.php
+++ tpl/system/detail.php
@@ -2,112 +2,141 @@
 <html>
   <head>
     <title><{$data.name}>  - API.com</title>
-    <{include file="system/meta.php"}>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0,minimum-scale=1.0,maximum-scale=2.0"/>
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="keywords" content="api, web service, lambda api"/>
+      <meta name="description"
+      content="<{$data.description}>"/>
     <{include file="system/style.php"}>
     <{include file="system/script.php"}>
   </head>
   <body>
     <!--header content-->
     <!-- load head tpl -->
-    <div style="background-color:#fff;margin:40px auto;width:70%;border:solid 1px #dedede;padding:40px;line-height:30px;font-size:14px;" >
+    <{include file="system/head.php"}>
+    <div style="margin:20px 50px;">
+        <div class="ui  grid">
+            <div class="eleven wide column" style="background-color:#fff;border:solid 1px #f6f6f6;padding:30px 50px;line-height:30px;font-size:14px;" >
 
-        <div style="border-bottom:solid 1px #f6f6f6;">
-            <a href="/" title="home">
-              <img  border="0"  title="api" alt="api" src="/img/logo.png"/>
-            </a>
+                <div style="border-bottom:solid 1px #f6f6f6;padding:10px;">
 
-        </div>
-        <div style="border-bottom:solid 1px #f6f6f6;padding:10px;">
+                    <a href="/" title="home" style="font-weight:bold;font-size:16px;color:#555;">
+                      <{$data.name}>
+                    </a>
+                </div>
+                <div class="ui small form segment" style="border:solid 2px #fff;box-shadow:none;">
 
-            <a href="/" title="home" style="font-weight:bold;font-size:16px;color:#555;">
-              <{$data.name}>
-            </a>
-        </div>
-        <div class="ui small form segment" style="border:solid 2px #fff;box-shadow:none;">
+                  <div class="two fields" >
+                      <div class="field">
+                            <label>Data Format:</label>
+                            <a class="ui blue label"><{$data.format}></a>
+                      </div>
+                      <div class="field">
+                          <label>Category:</label>
+                          <a class="ui label"><{$data.category.name}></a>
+                      </div>
 
-          <div class="two fields" >
-              <div class="field">
-                    <label>Data Format:</label>
-                    <a class="ui blue label"><{$data.format}></a>
-              </div>
-              <div class="field">
-                  <label>Category:</label>
-                  <a class="ui label"><{$data.category.name}></a>
-              </div>
-
-          </div>
-          <div class="two fields" >
-              <div class="field">
-                  <label>Price:</label>
-
-                  <{if $data.price_number == 0}>
-                    <a class="ui green label">Free</a>
-
-                  <{else}>
-                    <a class="ui red label"><{$data.price_number}><{$data.price_unit}> / Call</a>
-                  <{/if}>
-              </div>
-              <div class="field">
-                  <label>Discount:</label>
-                  <{if $data.discount_rate == 0}>
-                    <a class="ui label">None</a>
-                  <{else}>
-                    <a class="ui green label"><{$data.discount_rate}></a>
-                  <{/if}>
-              </div>
-          </div>
-          <div class="two fields" >
-              <div class="field">
-                  <label>Developer Token Required:</label>
-
-                  <{if $data.token_require == 0}>
-                    <a class="ui red label">Yes</a>
-                  <{else}>
-                    <a class="ui green label">no</a>
-                  <{/if}>
-              </div>
-              <div class="field">
-                  <label>Usage Quota:</label>
-                  <{if $data.usage_quota_time <= 0}>
-                    <a class="ui green label">Unlimit</a>
-                  <{else}>
-                    <a class="ui green label"><{$data.usage_quota_time|number_format}>/<{$data.usage_quota_unit}></a>
-                  <{/if}>
-              </div>
-          </div>
-          <div class="two fields" >
-              <div class="field">
-                  <label>Provider:</label>
-                  <a class="ui label"><{$data.provider_url}></a>
-              </div>
-              <div class="field">
-                  <label>License:</label>
-                  <a class="ui label"><{$data.license}></a>
-              </div>
-
-          </div>
-          <div class="one fields" >
-              <div class="field">
-                  <label>API URL:</label>
-                  <div class="ui label">
-                    <i class="linkify icon"></i>
-                    <a href="<{$data.api_url}>" target="_blank"><{$data.api_url}></a>
                   </div>
-              </div>
-          </div>
-          <div class="one fields" >
-              <div class="field">
-                  <label>Description:</label>
-                  <div class="ui label" style="font-size:14px;padding:20px;line-height:20px;width:90%;">
-                    <{$data.description}>
-                  </div>
-              </div>
-          </div>
+                  <div class="two fields" >
+                      <div class="field">
+                          <label>Price:</label>
 
+                          <{if $data.price_number == 0}>
+                            <a class="ui green label">Free</a>
+
+                          <{else}>
+                            <a class="ui red label"><{$data.price_number}><{$data.price_unit}> / Call</a>
+                          <{/if}>
+                      </div>
+                      <div class="field">
+                          <label>Discount:</label>
+                          <{if $data.discount_rate == 0}>
+                            <a class="ui label">None</a>
+                          <{else}>
+                            <a class="ui green label"><{$data.discount_rate}> Off</a>
+                          <{/if}>
+                      </div>
+                  </div>
+                  <div class="two fields" >
+                      <div class="field">
+                          <label>Provider:</label>
+                          <a class="ui label"><{$data.provider_url}></a>
+                      </div>
+                      <div class="field">
+                          <label>API URL:</label>
+                          <div class="ui label" >
+                            <i class="linkify icon"></i>
+                            <a style="text-overflow:ellipsis;overflow:hidden;" href="<{$data.api_url}>" target="_blank"><{$data.api_url}></a>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="one field" >
+                      <div class="field">
+                          <label>Description:</label>
+                          <div class="ui label" style="font-size:14px;padding:20px;line-height:20px;width:90%;">
+                            <{$data.description}>
+                          </div>
+                      </div>
+                  </div>
+
+                </div>
+            </div>
+            <div class="four wide column" style="background-color:#fff;border:solid 1px #f6f6f6;padding: 10px 20px;line-height:30px;font-size:14px;margin-left:10px;">
+                <div style="border-bottom:solid 1px #f6f6f6;padding:5px 10px;">
+
+                    <a   style="font-weight:bold;font-size:14px;color:#000;">
+                      Recommand APIs
+                    </a>
+                </div>
+                <div id="api-box" style="">
+
+                </div>
+            </div>
         </div>
     </div>
     <script type="text/javascript">
-      $('.ui.checkbox').checkbox();
+        $('.ui.checkbox').checkbox();
+        var category = <{$data.category.id}>;
+        var loading_code = '<div class="ui active inverted dimmer"><div class="ui text loader">Loading</div></div>';
+        var current_page = 1;
+        var page_size = 10;
+        var keyword = '';
+        $("#api-box").html(loading_code);
+        $.post("<{spUrl c=api a=getJsonData}>",
+            {'current_page':current_page, 'page_size':page_size, 'keyword':keyword, 'category':category},
+            function(response){
+                result = JSON.parse(response);
+                 if(result.code == 0){
+                  //loading data
+                  var apis = result.data;
+                   $("#api-box").html('');
+                   $('#page-box').html('');
+                   if (apis.length > 0) {
+                     for(var i = 0;i < apis.length;i++){
+                         var api_code = '<div style="width:100%;padding:10px;border-bottom:solid 1px #ededed;line-height:20px;"><div><a style="color:#555;font-weight:bold;" href="<{spUrl c=main a=detail}>?id=' + apis[i].id + '"> '+apis[i].name+'</a></div>';
+                         api_code += '<div class="provider" style="font-size:10px;text-overflow:ellipsis;overflow:hidden;"><a href="' + apis[i].api_url + '">' +  apis[i].api_url + '</a></div>';
+                         api_code += '<div class="provider" style="font-size:10px;color:#999;">Provided by :' + apis[i].provider_url + '</div>';
+                         api_code += '<div class="ui star rating" data-rating="0"></div>';
+
+                         api_code += '</div>';
+
+                        $("#api-box").append(api_code);
+                     }
+
+                   }else{
+                      var nodata_code = '<div style="margin:20px;"><span style="background-color:#fefed8;">';
+                      nodata_code += 'No result yet ';
+                      nodata_code += ' ...';
+                      nodata_code += '</span></div>';
+                      $("#api-box").append(nodata_code);
+
+                   }
+
+                 }else{
+                    $("#api-box").html("<li>Failed to load api data...</li>");
+                 }
+                 $('.ui.rating').rating();
+        });
     </script>
   </body>
 </html>
--- tpl/system/head.php
+++ tpl/system/head.php
@@ -2,8 +2,8 @@
     <div class="header" >
         <div class="ui  grid">
           <div class="four wide column">
-            <a href="/" title="home">
-              <img  border="0"  title="api" alt="api" src="/img/logo.png"/>
+            <a href="/" title="home" style="margin-left:20px;">
+              <img  border="0" height="45"  title="api" alt="api" src="/img/logo.png"/>
             </a>
           </div>
           <div class="eight wide column form" >
@@ -11,11 +11,11 @@
                 <input type="text" id="txt-keyword" placeholder="search..." />
                 <a class="ui icon mini button" href="javascript:search();" ><i class="search icon"></i></a>
             </div>
-             
+
 
           </div>
-          
-          <div class="four wide column " style="padding:25px;text-align:right;">
+
+          <div class="four wide column " style="padding:25px 60px;text-align:right;">
             <{if $smarty.session.user eq '' }>
               <a class="ui mini button default" href="<{spUrl c=main a=login}>" style="background-color: #fff;font-size:10px;">Log In</a>
               <a class="ui green mini button green-btn" href="<{spUrl c=main a=register}>" style="background-color: #00b040;font-size:10px;">Sign Up</a>
@@ -25,7 +25,7 @@
               </a>
                 <a class="btn" href="<{spUrl c=sub a=logout}>">exit</a>
             <{/if}>
-            
+
           </div>
         </div>
         <!--nav-head content-->
@@ -36,4 +36,3 @@
     transition: 'drop'
   });
 </script>
-    
